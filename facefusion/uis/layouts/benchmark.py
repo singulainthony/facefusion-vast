3d3586e481dce44e733d848091ce62d153c76297
singulainthony <singulainthony@proton.me>
2025-01-28 07:58:45 -0500

    Update benchmark.py

--- facefusion/uis/layouts/benchmark.py
+++ facefusion/uis/layouts/benchmark.py
@@ -87,4 +87,4 @@ def listen() -> None:
 
 
 def run(ui : gradio.Blocks) -> None:
-	ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser', share = 'True'))
+	ui.launch(favicon_path = 'facefusion.ico', share = 'True', inbrowser = state_manager.get_item('open_browser'))
